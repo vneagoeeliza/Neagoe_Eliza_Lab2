82d4e5201f2eee314cae6a06bdcc1e7b2683aa3e
vneagoeeliza <eliza.neagoe1@gmail.com>
2022-12-15 17:27:22 +0200

    Laborator4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Neagoe_Eliza_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
